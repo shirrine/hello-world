2bcfee6d779af19e8ca144366222b74bb311c535
Shirrine <shirrine@gmail.com>
2015-01-27 12:27:26 -0500

    Learning GitHub

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Learning GitHub
+ * 
+ * @author Shirrine
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * Hmm.... What to do here?
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+	}
+
+}
